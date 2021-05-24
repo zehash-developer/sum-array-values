732c4be2143aa5e614dc23b10e4957035fd18d73
Hashim <hashim.raza@mediamonks.com>
2021-05-24 13:39:57 +0300

    AVeryBigSum implementation finished.

--- sum-array-values.js
+++ sum-array-values.js
@@ -1,5 +1,15 @@
-const sumArrayValues = (array) => {
+// ES6 implementation of returning sum of all values in an array.
+// Unlike Java or C which have specific floating point types of size 2^32 -
+// JS standard let/var can support 2^53 values
+const sumArrayValues = (ar) => {
+  // Sum = 0 initial value.
   let sum = 0;
 
+  // Simple loop that goes over each value in the array and adds it to sum
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+
+  // return sum.
   return sum;
 };
